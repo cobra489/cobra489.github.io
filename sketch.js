cee3eb9a13119b67fb800af80467c5bc43a69bb0
Dorian Bručić <dor.brucic@gmail.com>
2023-12-17 22:25:11 +0100

    Webpage
    Offical 'Cobra489' lines of code.

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	//background(100,50,50);
+}
+
+function draw() {
+	
+}
+
+function windowResized() {
+	resizeCanvas(windowWidth, windowHeight);
+	//background(100,50,50);
+}
